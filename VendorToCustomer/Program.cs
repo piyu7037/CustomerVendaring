a76b1c7d9a2d08e592060689b297274c7610786b
Piyush Thorat <36068877+piyu7037@users.noreply.github.com>
2021-01-02 12:23:26 +0530

    CustomerVendor
    Project in asp.netcore

---
+++ VendorToCustomer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace VendorToCustomer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+              .ConfigureLogging((hostingContext, logging) =>
+              {
+                  // Requires `using Microsoft.Extensions.Logging;`
+                  logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                  logging.AddConsole();
+                  logging.AddDebug();
+                  logging.AddEventSourceLogger();
+                  logging.AddNLog();
+              })
+                .UseStartup<Startup>();
+    }
+}
